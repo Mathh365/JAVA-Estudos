96aeefb7dc1b930b82cda9d25e7d48f0f86ada11
Matheus Marcelo <matheuscrudi095@gmail.com>
2025-10-21 14:18:59 -0300

    Exercicios da aula4

---
+++ Nivel_1/Ex002.java
@@ -0,0 +1,23 @@
+//Desenvolva um algoritmo que informa se uma pessoa é criança,
+//adolescente, adulto ou idoso.
+
+import java.util.Scanner;
+
+public class Ex002 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int idade = 0;
+
+        System.out.print("Digite uma idade \n-");
+        idade = sc.nextInt();
+
+        if (idade <= 14){
+            System.out.println(idade + " É criança");
+        } else if (idade > 14 && idade <= 19){
+            System.out.println(idade + " É adolescente");
+        } else if (idade > 19){
+            System.out.println(idade + " É adulto");
+        }
+    }
+}
+
---
+++ Nivel_1/Ex006.java
@@ -0,0 +1,25 @@
+//Desenvolva um programa que pede um ano e determina se ele é bissexto ou não.
+
+import java.util.Scanner;
+
+public class Ex006 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int ano = 0;
+
+        System.out.print("Em que ano estamos?");
+        ano = sc.nextInt();
+
+        if (ano % 100 == 0){
+            if (ano % 400 == 0){
+                System.out.println("Este é um ano bissesxto");
+            } else {
+                System.out.println("Não é bissexto");
+            }
+        } else if (ano % 4 == 0) {
+            System.out.println("Este é um ano bissesxto");
+        } else {
+            System.out.println(String.format("%s não é bissexto", ano));
+        }
+    }
+}

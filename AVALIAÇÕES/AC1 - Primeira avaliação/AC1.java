5869f29e8a3832b71b9eeed3cd9e0cf467b327f3
Matheus Marcelo <matheuscrudi095@gmail.com>
2025-10-21 14:50:24 -0300

    Update AC1.java

--- AVALIAÇÕES/AC1 - Primeira avaliação/AC1.java
+++ AVALIAÇÕES/AC1 - Primeira avaliação/AC1.java
@@ -8,6 +8,7 @@ public class AC1 {
         Scanner sc = new Scanner(System.in);
         double ac1, ac2, ag, af, notaCorte, mediaFinal, mediaSemAG = 0;
 
+        //Recebe todos os valores
         System.out.println("Todas as notas devem estar entre 0 - 10");
         System.out.print("Digite a nota da AC1 \nNota:");
         ac1 = sc.nextDouble();
@@ -20,15 +21,18 @@ public class AC1 {
         System.out.print("Digite a nota de corte (o minimo necessario para passar)\nNota:");
         notaCorte = sc.nextDouble();
 
+        //Calcula a media final do aluno
         mediaFinal = (ac1 * 0.15) + (ac2 * 0.3) + (ag * 0.1) + (af * 0.45);
         System.out.println("Sua media e " + mediaFinal);
 
+        //Determina se o aluno foi aprovado ou nao com base na media final, e na nota que ele precisava (notaCorte), e em caso negativo, diz quanto faltou
         if (mediaFinal >= notaCorte){
             System.out.println("Parabens, voce foi aprovado");
         } else {
             System.out.println("Infelizmente voce foi reprovado, faltou " + (mediaFinal - notaCorte) + " para voce conseguir" );
         }
 
+        //Apresenta de maneira organizada todas as notas do aluno
         System.out.printf("""
                 Suas notas foram respectivamente:
                 AC1: %.2f
@@ -38,6 +42,7 @@ public class AC1 {
                 E sua nota de corte era: %.2f
                 """, ac1, ac2, ag, af, notaCorte);
 
+        //Esta parte diz respeito a um exercicio especifico da minha prova, onde eu deveria calcular a nota sem a AG, e aprsentar a diferença
         mediaSemAG = (ac1 * 0.15) + (ac2 * 0.3) + (af * 0.45);
         System.out.println("Caso sua nota da AG tivesse sido 0, sua nota seria seria: " + mediaSemAG + " com uma diferença de: " + (mediaFinal - mediaSemAG));
 

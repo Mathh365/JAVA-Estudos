7737a35ff004a594d49e4a093609d7a17c5ecd24
Matheus Marcelo <matheuscrudi095@gmail.com>
2025-10-22 23:59:57 -0300

    exercicios da aula 8

---
+++ Java/Nivel_4/Ex05.java
@@ -0,0 +1,24 @@
+//Crie um algoritmo que simula uma máquina de venda automática. O
+//usuário deve fornecer o valor do produto desejado e inserir o valor do
+//pagamento até que o pagamento seja suficiente para comprar o
+//produto.
+
+import java.util.Scanner;
+
+public class Ex05 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double valorProduto, pagamento = 0, somador = 0;
+
+        System.out.print("Digite o valor do produto que deseja comprar\nR$: ");
+        valorProduto = sc.nextInt();
+
+        do {
+            System.out.printf("Voce fez um pagamento no valor de R$ %.2f, falta %.2f\n", pagamento, (valorProduto - somador));
+            System.out.print("Qual o valor do proximo pagamento?\nR$: ");
+            pagamento = sc.nextDouble();
+            somador = somador + pagamento;
+        } while (somador < valorProduto);
+        System.out.println("Maravilha, voce kitou sua divida");
+    }
+}
